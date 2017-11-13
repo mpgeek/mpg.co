d043ac3395b297310de97fe82a206735d47ba488
mpgeek <multipurposegeek@gmail.com>
2017-11-13 11:54:20 -0800

    Simplify build directory.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ jekyll build --incremental
 
 ## Push intelligently.
 echo " >> Deploying ..."
-rsync -rtpvz --progress --checksum --delete-after --delete-excluded  _build/html/ mpgeek@mpg:/var/www/multipurposegeek.com/html
+rsync -rtpvz --progress --checksum --delete-after --delete-excluded  _build/ mpg:/var/www/multipurposegeek.com/html
 
 echo " ## COMPLETE!"
